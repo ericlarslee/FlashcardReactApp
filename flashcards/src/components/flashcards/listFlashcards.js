96f2b40f2b8603357b9aec0442786d71131d3c13
Eric Lars Lee <ericlarslee@gmail.com>
2021-05-13 17:57:44 -0500

    added a flip button that will flip over flashcard. also added functionality that will automatically show front side of card if next card is selected while current card is flipped over.

--- flashcards/src/components/flashcards/listFlashcards.js
+++ flashcards/src/components/flashcards/listFlashcards.js
@@ -19,17 +19,48 @@ const ListFlashcards = (props) => {
                 <Card.Body>
                 <div>
                     {props.filterFlashcards[props.index].question}
+                    <br />
+                    <button onClick={() => props.flip(!props.showFront)} >Flip</button>
+                    <br />
                 </div>
                 </Card.Body>
                 <Card.Footer>
                 <div>
+                    <br />
+                    <br />
                     <button onClick={() => props.backwardAction(props.index)} >Previous Card</button>
                     <button onClick={() => props.forwardAction(props.index)} >Next Card</button>
                 </div>
                 </Card.Footer>
             </Card>
         </div>
-    );
+    ); else;
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            {console.log('here' , props)}
+            <Card className='flashcard-card' style={{ alignContent: 'justify', background: 'red', border: '3px solid black', width: '280px', height: '200px', margin: '20px'}} >
+                <Card.Header>
+                    {props.currentCollectionName}
+                </Card.Header>
+                <Card.Body>
+                <div>
+                    {props.filterFlashcards[props.index].answer}
+                    <br />
+                    <button onClick={() => props.flip(!props.showFront)} >Flip</button>
+                    <br />
+                </div>
+                </Card.Body>
+                <Card.Footer>
+                <div>
+                    <br />
+                    <br />
+                    <button onClick={() => props.backwardAction(props.index)} >Previous Card</button>
+                    <button onClick={() => props.forwardAction(props.index)} >Next Card</button>
+                </div>
+                </Card.Footer>
+            </Card>
+        </div>
+    )
 }
 
 export default ListFlashcards;
